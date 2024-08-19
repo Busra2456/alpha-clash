67c776cdc97251e35c79c332cf920b469f7f8451
Busra2456 <you@example.com>
2024-08-19 16:21:15 +0600

    home section

--- jscode.js/AlphaClash.js
+++ jscode.js/AlphaClash.js
@@ -1,1 +1,13 @@
-console.log('tumi')
+// function play(){
+// //  step- 1: hide the home screen. to hidu the scereen add the class hidden to the home section
+// const homeSection =document.getElementById('home-screen');
+// // console.log(homeSection.classList)
+
+// homeSection.classList.add('hidden'); 
+
+
+// // show the playground 
+// const playGround = document.getElementById('play-ground');
+// playGround.classList.remove('hidden')
+      
+// }
